7407596171ec956cda46a8306d932bb544dd9b6f
Aamir Khan <sengoku264@gmail.com>
2026-06-13 18:46:30 +0530

    backend running

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import { rateLimit } from "express-rate-limit";
+import { connectDB } from "./src/config/db.js"
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const app = express()
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window`
+    message: "Too many requests from this IP, please try again later."
+});
+
+app.use(express.json())
+app.use(cors())
+app.use(helmet())
+app.use(morgan("dev"))
+app.use(limiter)
+
+app.get("/", (req,res) => {
+    res.send("Backend is fine!")
+})
+
+const PORT = process.env.PORT || 3000;
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    })
+});
---
+++ backend/src/config/db.js
@@ -0,0 +1,21 @@
+import pkg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const { Pool} = pkg;
+
+export const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+});
+
+export const connectDB = async () => {
+    try {
+        const client = await pool.connect();
+        console.log("Database connected successfully");
+        client.release();
+    } catch (err) {
+        console.error("Database connection error:", err.message);
+        process.exit(1);
+    }
+};
